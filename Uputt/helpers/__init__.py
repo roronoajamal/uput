1239bc715bb881caea1629c609c26624f624f119
dibonihbos <147966859+dibonihbos@users.noreply.github.com>
2023-11-05 04:24:06 +0700

    Update __init__.py

--- Uputt/helpers/__init__.py
+++ Uputt/helpers/__init__.py
@@ -25,8 +25,7 @@ async def join(client):
         await client.join_chat("UputtSupport")
         await client.join_chat("Flukosaa")
         await client.join_chat("kynansupport")
-        await client.join_chat("t.me/+WJ7jDmCqQCBkMmY9")
-        await client.join_chat("kane_reborn")
-        await client.join_chat("pesulaptelegram")
+        await client.join_chat("dibonihbosss")
+        await client.join_chat("nicetryxsellerkeren")
     except BaseException:
         pass
